a19c479adb8e29e717d8e929b34236971d644be9
dmn-05 <nnbdo45@gmail.com>
2026-01-06 21:27:02 +0700

    Rename Project

--- DoAnMonHoc/DoAnMonHoc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DoAnMonHoc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
