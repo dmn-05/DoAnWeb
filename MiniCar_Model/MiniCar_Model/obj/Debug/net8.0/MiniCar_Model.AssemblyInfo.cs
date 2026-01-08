9d8b9ece7d7afde9e6d19351fd0d3c9e120b4826
dmn-05 <nnbdo45@gmail.com>
2026-01-08 09:57:04 +0700

    Fix css

--- MiniCar_Model/MiniCar_Model/obj/Debug/net8.0/MiniCar_Model.AssemblyInfo.cs
+++ MiniCar_Model/MiniCar_Model/obj/Debug/net8.0/MiniCar_Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniCar_Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aabd6d08db929df524ec1cbab617aa6720c7faf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574e1a672de782f105e01878f6f6ad0aa98ebe3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniCar_Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniCar_Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
