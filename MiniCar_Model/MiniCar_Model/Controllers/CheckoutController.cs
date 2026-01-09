21848060e8a88c147bd50f661040c298d113c36a
DoNgocThe2005 <0306231335@caothang.edu.vn>
2026-01-09 23:49:51 +0700

    Add Cart, Checkout UI

---
+++ MiniCar_Model/MiniCar_Model/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCar_Model.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
