01b3cc3324e2fa9796ec664cb7f6d664efc406e8
dmn-05 <nnbdo45@gmail.com>
2026-01-09 12:29:13 +0700

    update Interface

---
+++ MiniCar_Model/MiniCar_Model/Controllers/CheckoutController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCar_Model.Controllers {
+  public class CheckoutController : Controller {
+    public IActionResult Index() {
+      return View();
+    }
+  }
+}
