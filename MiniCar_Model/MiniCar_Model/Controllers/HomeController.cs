a9602191d44398d0e4684207589e40f1e1ab00aa
TRIVIPZ <minhhtrii2005@gmail.com>
2026-01-15 11:12:13 +0700

    Create contact function and Fix the create product page

--- MiniCar_Model/MiniCar_Model/Controllers/HomeController.cs
+++ MiniCar_Model/MiniCar_Model/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MiniCar_Model.Models;
 
 namespace MiniCar_Model.Controllers {
   public class HomeController : Controller {
     private readonly ILogger<HomeController> _logger;
-
+    //------Tri Trong Treo
+    //private readonly ApplicationDBContext _context;
+    //------Tri Trong Treo
     public HomeController(ILogger<HomeController> logger) {
       _logger = logger;
     }
@@ -13,10 +15,30 @@ namespace MiniCar_Model.Controllers {
     public IActionResult Index() {
       return View();
     }
-
-    public IActionResult Contact() {
+    
+    //------Tri Trong Treo
+    //Xu ly trang lien he tu khach hang
+    [HttpGet]
+    public IActionResult Contact() 
+    {
       return View();
     }
+
+    //[HttpPost]
+    //public async Task<IActionResult> Contact(Contact contact) 
+    //{ 
+    //    if (ModelState.IsValid)
+    //    {
+    //        _context.Contacts.Add(contact);
+    //        await _context.SaveChangesAsync();
+
+    //        ViewBag.Success = "Cảm ơn bạn đã liên hệ với chúng tôi";
+    //        ModelState.Clear();
+    //        return View();
+    //    }
+    //    return View(contact);
+    //}
+    //------Tri Trong Treo
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() {
